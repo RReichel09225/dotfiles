3ffe15682a5dbc26e83b6cf750756af472f09030
Serge Gebhardt <serge.gebhardt@gmail.com>
2015-09-22 10:59:48 +0200

    zsh: override gbL to show upstream branch

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -46,9 +46,10 @@ alias gwCx='git clean -fdx'
 # Create an empty branch
 alias gbC='git checkout --orphan'
 
-# Override `gbl` to show upstream tracking branch name as well
+# Override `gbl` and `gbL` to show upstream tracking branch name as well
 # default is to alias to 'git branch -v', not showing upstream
 alias gbl='git branch -vv'
+alias gbL='git branch -avv'
 
 # By default zprezto aliases gpf to git push --force
 # I find this too dangerous and prefer explicitly passing --force
