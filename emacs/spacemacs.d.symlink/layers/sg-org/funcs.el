20be989f6798a13b2a20be187e3111dfcda37e88
Serge Gebhardt <serge.gebhardt@gmail.com>
2018-08-09 21:41:27 +0200

    emacs: sg-org moves org-capture-frame to screen center

--- emacs/spacemacs.d.symlink/layers/sg-org/funcs.el
+++ emacs/spacemacs.d.symlink/layers/sg-org/funcs.el
@@ -5,23 +5,37 @@
 ;; Bind key to: emacsclient --eval "(sg/make-org-capture-frame)"
 ;; For example with Keyboard Maestro, Alfred, Quicksilver, OSX AppleScript services, XMonad, etc
 
+(provide 'sg-org)
+
 (defun sg/make-org-capture-frame ()
   "Create a new frame and run `org-capture'."
   (interactive)
   (select-frame (make-frame '((sg/org-capture-frame . t))))
-  ;; x-focus-frame might be necessary only for osx
+  (sg/move-frame-center)
   (x-focus-frame nil)
   (delete-other-windows)
   (cl-letf (((symbol-function 'switch-to-buffer-other-window) #'switch-to-buffer))
     (condition-case err
         (org-capture)
-      ;; Delete the newly-created frame in case of any error: typing "q", choosing an illegal template key,
-      ;; aborting with C-c C-k all generate errors (among other error cases).
+      ;; Delete the newly-created frame in case of any error: typing "q", choosing an illegal
+      ;; template key, aborting with C-c C-k all generate errors (among other error cases).
       (error (delete-frame)))))
 
-(defun sg/delete-capture-frame ()
+(defun sg/delete-org-capture-frame ()
   "Delete the newly-created `org-mode' frame."
   (when (frame-parameter nil 'sg/org-capture-frame)
     (delete-frame)))
 
-(add-hook 'org-capture-after-finalize-hook 'sg/delete-capture-frame)
+(add-hook 'org-capture-after-finalize-hook 'sg/delete-org-capture-frame)
+
+(defun sg/move-frame-center ()
+  "Move the selected frame to the center of the display."
+  (interactive)
+  (let* ((FHEIGHT (frame-pixel-height))
+         (FWIDTH  (frame-pixel-width))
+         (DHEIGHT (x-display-pixel-height))
+         (DWIDTH  (x-display-pixel-width)))
+    ;; (set-frame-size (selected-frame) (/ WIDTH 3) HEIGHT t)
+    (set-frame-position (selected-frame)
+                        (- (/ DWIDTH 2) (/ FWIDTH 2))
+                        (- (/ DHEIGHT 2) (/ FHEIGHT 2)))))
